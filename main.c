cb0f22923e7f3bf8461bcdf42249776e1611427f
Baptiste <baptdelaverny@gmail.com>
2023-10-27 19:42:32 +0200

    Début création des murs, bug avec les murs en colonnes pour je ne sais quelconque raisons mais les lignes fonctionnent

--- main.c
+++ main.c
@@ -5,50 +5,62 @@
 #define clear() printf("\033[H\033[J")
 
 char perso = 'X'; //Notre perso pac man pour l'instant
-char table[20][40]; //Notre arène
+char table[40][60]; //Notre arène
 
 //Initialisation des variables
-int x=1;
-int y=1;
+int x=31;
+int y=21;
 int i,j;
 
 
 void arene(){ // Création de l'arène
 //bord du haut
-for(j=0;j<40;j++){
+for(j=0;j<60;j++){
    table[0][j]='_';
   }
 
 //bord du bas
-for(j=0;j<40;j++){
-   table[19][j]='_';
+for(j=0;j<60;j++){
+   table[39][j]='_';
   }
 
 //bord de gauche
-for(i=1;i<19;i++){
+for(i=1;i<39;i++){
    table[i][0]='|';
   }
 
 //bord de droite
-for(i=1;i<19;i++){
-   table[i][39]='|';
+for(i=1;i<39;i++){
+   table[i][59]='|';
   }
 
 //interieur
-for(i=1;i<19;i++){
-  for(j=1;j<39;j++){
+for(i=1;i<39;i++){
+  for(j=1;j<59;j++){
    table[i][j]='.';
   }
 
 //mur
- }
+for(j=28;j<33;j++){
+ table[22][j]='_';
+}
+
+for(j=28;j<33;j++){
+ table[19][j]='_';
+}
+
+for(i=19;i<23;i++){
+   table[i][28]='|';
+  }
 
 table[y][x]=perso;
 }
+}
+
 
 void afficher(){
-for(i=0;i<20;i++){
- for(j=0;j<40;j++){
+for(i=0;i<40;i++){
+ for(j=0;j<60;j++){
    printf("%c",table[i][j]);
   }
    printf("\n");
@@ -64,7 +76,7 @@ int main(void){
  afficher();
 
  char choixd;
- char c;
+
 
  while(-1){
   printf("\n\n[z(Haut), s(Bas), d(Droite), q(Gauche)]?\n");
@@ -78,7 +90,6 @@ int main(void){
    break;
 
    case'q':{table[y][x]=' ';x--;}
-   
    break;
 
    case'd':{table[y][x]=' ';x++;}
@@ -89,15 +100,6 @@ int main(void){
 
 
   }
-
-  while(1) {
-   printf("%c",c);
-   fflush(stdout);
-   sleep(1);
-   clear();
-   c++;
-   if(c>126) c=33;
-   }
 
   arene();
   afficher();
